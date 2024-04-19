089b60180c75ef44a665593fc23ca26730cddf78
KaynVR <varyzendvs@gmail.com>
2024-04-18 22:56:14 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const btnBorrar = document.querySelector('#btn-borrar');
+const btn1 = document.querySelector('#btn-1');
+const btn2 = document.querySelector('#btn-2');
+const btn3 = document.querySelector('#btn-3');
+const btn4 = document.querySelector('#btn-4');
+const btn5 = document.querySelector('#btn-5');
+const btn6 = document.querySelector('#btn-6');
+const btn7 = document.querySelector('#btn-7');
+const btn8 = document.querySelector('#btn-8');
+const btn9 = document.querySelector('#btn-9');
+const btn0 = document.querySelector('#btn-0');
+const btnPorcentaje = document.querySelector('#btn-porcentaje');
+const btnDividir = document.querySelector('#btn-dividir');
+const btnMultiplicar = document.querySelector('#btn-multiplicar');
+const btnReset = document.querySelector('#btn-reset');
+const btnRestar = document.querySelector('#btn-restar');
+const btnSumar = document.querySelector('#btn-sumar');
+const btnDecimal = document.querySelector('#btn-punto');
+const btnIgual = document.querySelector('#btn-igual');
+
+let n = (btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9);
+
+btn0 = 0;
+btn1 = 1;
+btn2 = 2;
+btn3 = 3;
+btn4 = 4;
+btn5 = 5;
+btn6 = 6;
+btn7 = 7;
+btn8 = 8;
+btn9 = 9;
+btnDecimal = (n.n)
+
+btnIgual.addEventListener('click', (e) => {
+    e.preventDefault();
+
+})
+
+
+
